c92e3592e672fc9a87cca51444745b1436c35423
Adil Moujahid <adil.mouja@gmail.com>
2014-07-08 22:31:38 +0900

    First commit: Blog structure created, About page and first post added

---
+++ content/pages/about.md
@@ -0,0 +1,11 @@
+title: About
+
+This is a personal blog I've started to share share thoughts on data analytics and applied mathematics.
+
+I am a data scientist in NTT DATA Corporation Japan. I work on building Big Data infrastructure solutions and data analytics algorithms. I also work on business analytics projects for different vertical industries.
+
+I hold a masters degree in Information Science and Electrical Engineering from Kyushu University - Japan. My undergraduate thesis was about the application of algorithms from information theory to financial data. During my graduate years, I worked on theoretical machine learning as well as practical application of machine learning techniques to data compression.
+
+I also participate on a regular basis to data analytics competitions run by Kaggle.com. [Kaggle profile](https://www.kaggle.com/users/52887/adil) 
+
+*Disclaimer: All information presented in this site does not represent the thoughts, intentions, plans or strategies of any corporation, business or company. It is solely my opinion.*
---
+++ pelicanconf.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Adil Moujahid'
+SITENAME = u'Adil Moujahid'
+TAGLINE = u'Data Analytics and more'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Tokyo'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+
+MENUITEMS = [('Archive', 'archives.html'), ('About', 'pages/about.html'), ]
+
+STATIC_PATHS = ['images', 'pdfs']
+
+COVER_IMG_URL = '/images/cover-img.jpg'
+
+# Social widget
+SOCIAL = (('twitter', 'https://twitter.com/AdilMouja'),
+          ('linkedin', 'https://www.linkedin.com/pub/adil-moujahid/65/51/83a'),
+          ('github', 'https://github.com/adilmoujahid'),)
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+
+DEFAULT_PAGINATION = 10
+
+# Show most recent posts first
+NEWEST_FIRST_ARCHIVES = False
+
+# Specify theme
+THEME = "theme/pure"
+
+# Plugins
+PLUGIN_PATH = 'plugins'
+PLUGINS = ['gravatar']
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
