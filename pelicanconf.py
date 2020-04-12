dfd3ff389024db293b5a7b155fa034f9b7ece93a
Adil Moujahid <adil.mouja@gmail.com>
2020-04-12 19:20:39 +0200

    New Blog Post: Analyzing the Impact of Coronavirus on the Stock Market

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,9 +2,6 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
 
 AUTHOR = u'Adil Moujahid'
 SITENAME = u'Adil Moujahid'
