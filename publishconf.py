b5846e1f880ef47787dd1a28cceed0a5a06d2070
Adil Moujahid <adil.mouja@gmail.com>
2023-05-01 20:50:31 +0200

    Configured blog to use HTTPS

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://adilmoujahid.com'
+SITEURL = 'https://adilmoujahid.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
